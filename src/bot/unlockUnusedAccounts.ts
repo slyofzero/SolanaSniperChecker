247f9335a76ff17c58f5a2d1814a6cf8d6fee814
Ishan Shishodiya <sly.of.zero@gmail.com>
2024-02-18 21:38:21 +0530

    fixes

---
+++ src/bot/unlockUnusedAccounts.ts
@@ -0,0 +1,32 @@
+import { getDocument, updateDocumentById } from "@/firebase";
+import { solanaConnection } from "@/rpc";
+import { StoredAccount } from "@/types/accounts";
+import { decrypt } from "@/utils/cryptography";
+import { errorHandler, log } from "@/utils/handlers";
+import { Keypair } from "@solana/web3.js";
+
+export async function unlockUnusedAccounts() {
+  const lockedAccounts = (await getDocument({
+    collectionName: "accounts",
+    queries: [["locked", "==", true]],
+  })) as StoredAccount[];
+
+  for (const { id, secretKey } of lockedAccounts) {
+    try {
+      const account = Keypair.fromSecretKey(
+        new Uint8Array(JSON.parse(decrypt(secretKey)))
+      );
+      const balance = await solanaConnection.getBalance(account.publicKey);
+
+      if (balance === 0) {
+        updateDocumentById({
+          updates: { locked: false, lockedAt: null },
+          collectionName: "accounts",
+          id: id || "",
+        }).then(() => log(`Unlocked account ${id}`));
+      }
+    } catch (error) {
+      errorHandler(error);
+    }
+  }
+}
