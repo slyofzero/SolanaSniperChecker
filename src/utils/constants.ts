16e991493b536a4436f7dbf1f0674f7294d73ce4
Ishan Shishodiya <sly.of.zero@gmail.com>
2024-02-22 15:25:39 +0530

    fixes

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 // export const VOLUME_THRESHOLD = 1000;
 // export const LIQUIDITY_THRESHOLD = 1;
 export const VOLUME_THRESHOLD = 10000;
-export const LIQUIDITY_THRESHOLD = 15;
+export const LIQUIDITY_THRESHOLD = 7;
 export const CHECK_INTERVAL = 5 * 60;
 export const CLEANUP_INTERVAL = 30;
 export const MAX_START_TIME = 60 * 10;
